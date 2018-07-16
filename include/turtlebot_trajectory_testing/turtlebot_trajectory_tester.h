6b828fedbccad54ec0265340e13870d6d8e06517
Justin Smith <jssmith@gatech.edu>
2018-07-16 18:38:39 -0400

    Without extern instantiation, takes 1:22 to recompile after insignificant change to depth_controller

--- include/turtlebot_trajectory_testing/turtlebot_trajectory_tester.h
+++ include/turtlebot_trajectory_testing/turtlebot_trajectory_tester.h
@@ -4,6 +4,9 @@
 #include <pips_trajectory_testing/pips_trajectory_tester.h>
 #include <turtlebot_trajectory_generator/near_identity.h>
 
+//extern template class GenAndTest<ni_state, ni_controller>;
+
 typedef GenAndTest<ni_state, ni_controller> TurtlebotGenAndTest;
+
 
 #endif //TURTLEBOT_TRAJECTORY_TESTER
